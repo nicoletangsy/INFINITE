1aa80d7f53446a663c1b76f627d88b1739e54489
Nicole Tang <nicoletangsy@gmail.com>
2019-02-13 21:29:50 +0800

    edit original musicsheet

--- app/src/main/java/comnicoletangsyinfinite/httpsgithub/infinite/RightHandFeedback.java
+++ app/src/main/java/comnicoletangsyinfinite/httpsgithub/infinite/RightHandFeedback.java
@@ -24,7 +24,7 @@ public class RightHandFeedback extends AppCompatActivity {
         Button nextPageButton = findViewById(R.id.nextPageButton);
         TextView pageNubmer = findViewById(R.id.pageNumber);
         TextView original = findViewById(R.id.original2);
-        String oNotes = "E4 D4 C4 D4 E4 E4 E4 E4 D4 D4 D4 D4 E4 G4 G4 G4 ";
+        String oNotes = "E4 D4 C4 D4 E4 G4 A4 F4 C5 B4 A4 G4 F4 E4 B4 C4 ";
         original.setText("" + oNotes);
         TextView result = findViewById(R.id.result2);
         result.setText("result: " + aMusicNotes.getList());
