d6ca18433407bb1651e8634405bd980f65a0069d
mcc <mccins28@gmail.com>
2019-04-28 16:49:09 +0800

    added tutorial UI and changed main UI

--- app/src/main/java/comnicoletangsyinfinite/httpsgithub/infinite/AboutActivity.java
+++ app/src/main/java/comnicoletangsyinfinite/httpsgithub/infinite/AboutActivity.java
@@ -4,8 +4,9 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
-
 public class AboutActivity extends AppCompatActivity {
 
     @Override
@@ -19,6 +20,14 @@ public class AboutActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayShowHomeEnabled(true);
     }
 
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem menuItem) {
         if (menuItem.getItemId() == android.R.id.home) {
@@ -28,5 +37,6 @@ public class AboutActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(menuItem);
     }
+
 
 }
